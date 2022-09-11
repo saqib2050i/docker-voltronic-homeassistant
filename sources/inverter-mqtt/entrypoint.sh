a00bac64e8ab1ced2f681b93873835159fba0960
saqib2050i <44846585+saqib2050i@users.noreply.github.com>
2022-09-11 21:31:07 +0500

    Update entrypoint.sh
    reset

--- sources/inverter-mqtt/entrypoint.sh
+++ sources/inverter-mqtt/entrypoint.sh
@@ -6,10 +6,10 @@ export TERM=xterm
 # Init the mqtt server for the first time, then every 5 minutes
 # This will re-create the auto-created topics in the MQTT server if HA is restarted...
 
-(while :; do /opt/inverter-mqtt/mqtt-init.sh; sleep 300; done) & 
+watch -n 300 /opt/inverter-mqtt/mqtt-init.sh > /dev/null 2>&1 &
 
 # Run the MQTT Subscriber process in the background (so that way we can change the configuration on the inverter from home assistant)
-(while :; do /opt/inverter-mqtt/mqtt-subscriber.sh; sleep 1; done) &
+/opt/inverter-mqtt/mqtt-subscriber.sh &
 
 # execute exactly every 30 seconds...
-while :; do /opt/inverter-mqtt/mqtt-push.sh; sleep 4; done
+watch -n 30 /opt/inverter-mqtt/mqtt-push.sh > /dev/null 2>&1
