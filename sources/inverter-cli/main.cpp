26e0df05f8e77a53abbf79d9aa24a920d6027bc7
saqib2050i <44846585+saqib2050i@users.noreply.github.com>
2024-05-16 01:28:37 +0500

    Update main.cpp

--- sources/inverter-cli/main.cpp
+++ sources/inverter-cli/main.cpp
@@ -312,8 +312,8 @@ int main(int argc, char* argv[]) {
                 printf("  \"Out_source_priority\":%d,\n", out_source_priority);
                 printf("  \"Charger_source_priority\":%d,\n", charger_source_priority);
                 printf("  \"Battery_redischarge_voltage\":%.1f,\n", batt_redischarge_voltage);
-                printf("  \"Warnings\":\"%s\"\n", warnings->c_str());
-                printf("  \"Model\":\"%s\"\n", generalmodel->c_str());
+                printf("  \"Warnings\":\"%s\",\n", warnings->c_str());
+                printf("  \"Model\":\"%s\",\n", generalmodel->c_str());
                 printf("  \"Qflag\":\"%s\"\n", qflag->c_str());
                 printf("}\n");
 
