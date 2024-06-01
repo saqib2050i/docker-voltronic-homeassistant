2253ad3cdd0544a09ff43d8e750966441d099e03
akoii <akoiii123@gmail.com>
2024-06-02 01:15:29 +0500

    Update inverter.cpp

--- sources/inverter-cli/inverter.cpp
+++ sources/inverter-cli/inverter.cpp
@@ -87,119 +87,136 @@ int cInverter::GetMode() {
 }
 
 bool cInverter::query(const char *cmd) {
-    time_t started;
-    int fd;
-    int i=0, n;
+  time_t started;
+  int fd;
+  int i = 0, n;
 
-    fd = open(this->device.data(), O_RDWR | O_NONBLOCK);
-    if (fd == -1) {
-        lprintf("INVERTER: Unable to open device file (errno=%d %s)", errno, strerror(errno));
-        sleep(5);
-        return false;
+  fd = open(this->device.data(), O_RDWR | O_NONBLOCK);  // device is provided by program arg (usually /dev/hidraw0)
+  if (fd == -1) {
+    lprintf("DEBUG:  Unable to open device file (errno=%d %s)", errno, strerror(errno));
+    sleep(10);
+    return false;
+  }
+
+  // Once connected, set the baud rate and other serial config (Don't rely on this being correct on the system by default...)
+  speed_t baud = B2400;
+
+  // Speed settings (in this case, 2400 8N1)
+  struct termios settings;
+  tcgetattr(fd, &settings);
+
+  cfsetospeed(&settings, baud);      // baud rate
+  settings.c_cflag &= ~PARENB;       // no parity
+  settings.c_cflag &= ~CSTOPB;       // 1 stop bit
+  settings.c_cflag &= ~CSIZE;
+  settings.c_cflag |= CS8 | CLOCAL;  // 8 bits
+  // settings.c_lflag = ICANON;         // canonical mode
+  settings.c_oflag &= ~OPOST;        // raw output
+
+  tcsetattr(fd, TCSANOW, &settings); // apply the settings
+  tcflush(fd, TCOFLUSH);
+
+  // ---------------------------------------------------------------
+
+  // Generating CRC for a command
+  uint16_t crc = cal_crc_half((uint8_t*)cmd, strlen(cmd));
+  n = strlen(cmd);
+  memcpy(&buf, cmd, n);
+  lprintf("DEBUG:  Current CRC: %X %X", crc >> 8, crc & 0xff);
+  buf[n++] = crc >> 8;
+  buf[n++] = crc & 0xff;
+  buf[n++] = 0x0d;
+
+  // Send buffer in hex
+  char messagestart[128];
+  char* messageptr = messagestart;
+  sprintf(messagestart, "DEBUG:  Send buffer hex bytes:  ( ");
+  messageptr += strlen(messagestart);
+
+  for (int j = 0; j < n; j++) {
+    int size = sprintf(messageptr, "%02x ", buf[j]);
+    messageptr += 3;
+  }
+  lprintf("%s)", messagestart);
+
+  /* The below command doesn't take more than an 8-byte payload 5 chars (+ 3
+     bytes of <CRC><CRC><CR>).  It has to do with low speed USB specifications.
+     So we must chunk up the data and send it in a loop 8 bytes at a time.  */
+
+  // Send the command (or part of the command if longer than chunk_size)
+  int chunk_size = 8;
+  if (n < chunk_size) // Send in chunks of 8 bytes, if less than 8 bytes to send... just send that
+    chunk_size = n;
+  int bytes_sent = 0;
+  int remaining = n;
+
+  while (remaining > 0) {
+    ssize_t written = write(fd, &buf[bytes_sent], chunk_size);
+    bytes_sent += written;
+    if (remaining - written >= 0)
+      remaining -= written;
+    else
+      remaining = 0;
+
+    if (written < 0)
+      lprintf("DEBUG:  Write command failed, error number %d was returned", errno);
+    else
+      lprintf("DEBUG:  %d bytes written, %d bytes sent, %d bytes remaining", written, bytes_sent, remaining);
+
+    chunk_size = remaining;
+    usleep(50000);   // Sleep 50ms before sending another 8 bytes of info
+  }
+
+  time(&started);
+
+  // Instead of using a fixed size for expected response length, lets find it
+  // by searching for the first returned <cr> char instead.
+  char *startbuf = 0;
+  char *endbuf = 0;
+  do {
+    // According to protocol manual, it appears no query should ever exceed 120 byte size in response
+    n = read(fd, (void*)buf+i, 120 - i);
+    if (n < 0)
+    {
+      if (time(NULL) - started > 5)     // Wait 5 secs before timeout
+      {
+        lprintf("DEBUG:  %s read timeout", cmd);
+        break;
+      }
+      else
+      {
+        usleep(50000);  // sleep 50ms
+        continue;
+      }
     }
+    i += n;
+    buf[i] = '\0';  // terminate what we have so far with a null string
+    lprintf("DEBUG:  %d bytes read, %d total bytes:  %02x %02x %02x %02x %02x %02x %02x %02x",
+            n, i, buf[i-8], buf[i-7], buf[i-6], buf[i-5], buf[i-4], buf[i-3], buf[i-2], buf[i-1]);
 
-
-    // Once connected, set the baud rate and other serial config (Don't rely on this being correct on the system by default...)
-    speed_t baud = B2400;
-
-    // Speed settings (in this case, 2400 8N1)
-    struct termios settings;
-    tcgetattr(fd, &settings);
-
-    cfsetospeed(&settings, baud);      // baud rate
-    settings.c_cflag &= ~PARENB;       // no parity
-    settings.c_cflag &= ~CSTOPB;       // 1 stop bit
-    settings.c_cflag &= ~CSIZE;
-    settings.c_cflag |= CS8 | CLOCAL;  // 8 bits
-    // settings.c_lflag = ICANON;         // canonical mode
-    settings.c_oflag &= ~OPOST;        // raw output
-
-    tcsetattr(fd, TCSANOW, &settings); // apply the settings
-    tcflush(fd, TCOFLUSH);
-
-    // ---------------------------------------------------------------
-
-    // Generating CRC for a command
-    uint16_t crc = cal_crc_half((uint8_t*)cmd, strlen(cmd));
-    n = strlen(cmd);
-    memcpy(&buf, cmd, n);
-    lprintf("INVERTER: Current CRC: %X %X", crc >> 8, crc & 0xff);
-
-    buf[n++] = crc >> 8;
-    buf[n++] = crc & 0xff;
-    //buf[n++] = 0x0d;
-    buf[n++] = 0x0d; // '\r'
-    buf[n+1] = '\0'; // see workaround below
-
-    // send a command
-    int chunk_size = 8;
-    for (int offset = 0; offset < n; usleep(50000)) {
-	int left = n - offset;
-	int towrite = left > chunk_size ? chunk_size : left;
-	// WORKAROUND: For some reason, writing 1 byte causes it to error.
-	// However, since we padded with '\0' above, we can give it 2 instead.
-	// I don't know of any 6 (+ 2*CRC + '\r') byte commands to test it on
-	// but this at least gets it to return NAK.
-	if (towrite == 1) towrite = 2;
-        lprintf("DEBUG: offset %d, writing %d", offset, towrite);
-	ssize_t written = write(fd, &buf[offset], towrite);
-	if (written > 0)
-	    offset += written;
-	else {
-	    lprintf("INVERTER: command write failed (written=%d, errno=%d: %s)", written, errno, strerror(errno));
-	    break;
-	}
-        lprintf("DEBUG: %d bytes to write, %d bytes written", n, offset);
-    }
-    
-    
-    
-    //send a command
-    char *startbuf = 0;
-    char *endbuf = 0;
-    time(&started);
-    do {
-        n = read(fd, &buf[i], 120-i);
-        if (n < 0) {
-            if (time(NULL) - started > 2) {
-                lprintf("INVERTER: %s read timeout", cmd);
-                break;
-            } else {
-                usleep(10);
-                continue;
-            }
-        }
-
-        i += n;
     startbuf = (char *)&buf[0];
-	endbuf = strchr(startbuf, '\r');
-    } while (endbuf == NULL);
-    close(fd);
-    buf[i] = '\0';
+    endbuf = strchr(startbuf, '\r');
 
-     if (endbuf != NULL) {
-        int replysize = endbuf - startbuf + 1;
+    //lprintf("DEBUG:  %s Current buffer: %s", cmd, startbuf);
+  } while (endbuf == NULL);     // Still haven't found end <cr> char as long as pointer is null
+  close(fd);
 
-        lprintf("INVERTER: %s reply size (%d bytes)", cmd, i);
+  int replysize = endbuf - startbuf + 1;
+  lprintf("DEBUG:  Found reply <cr> at byte: %d", replysize);
 
-        if (buf[0]!='(' || buf[replysize-1]!=0x0d) {
-            lprintf("INVERTER: %s: incorrect start/stop bytes.  Buffer: %s", cmd, buf);
-            return false;
-        }
-        if (!(CheckCRC(buf, replysize))) {
-            lprintf("INVERTER: %s: CRC Failed!  Reply size: %d  Buffer: %s", cmd, replysize, buf);
-            return false;
-        }
-
-        buf[replysize-3] = '\0'; // null terminating on first CRC byte
-        lprintf("INVERTER: %s: %d bytes read: %s", cmd, i, buf);
-
-        lprintf("INVERTER: %s query finished", cmd);
-        return true;
-    } else {
-        lprintf("INVERTER: %s couldn't find reply <cr> (%d bytes)", cmd, i);
-        return false;
-    }
+  if (buf[0]!='(' || buf[replysize-1]!=0x0d) {
+    lprintf("DEBUG:  %s: incorrect buffer start/stop bytes.  Buffer: %s", cmd, buf);
+    return false;
+  }
+  if (!(CheckCRC(buf, replysize))) {
+    lprintf("DEBUG:  %s: CRC Failed!  Reply size: %d  Buffer: %s", cmd, replysize, buf);
+    return false;
+  }
+  buf[replysize-3] = '\0';      // Null-terminating on first CRC byte
+  lprintf("DEBUG:  %s: %d bytes read: %s", cmd, i, buf);
+  
+  lprintf("DEBUG:  %s query finished", cmd);
+  return true;
 }
 
 void cInverter::poll() {
