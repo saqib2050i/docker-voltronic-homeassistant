941edd77f2efe7dec1614e822852900e37cb0b57
saqib2050i <44846585+saqib2050i@users.noreply.github.com>
2024-06-04 13:38:31 +0500

    Update inverter.cpp

--- sources/inverter-cli/inverter.cpp
+++ sources/inverter-cli/inverter.cpp
@@ -87,7 +87,7 @@ int cInverter::GetMode() {
 }
 
 bool cInverter::query(const char *cmd) {
-  time_t started;
+    time_t started;
     int fd;
     int i=0, n;
 
@@ -129,25 +129,9 @@ bool cInverter::query(const char *cmd) {
     buf[n++] = crc & 0xff;
     //buf[n++] = 0x0d;
     buf[n++] = 0x0d; // '\r'
-    buf[n+1] = '\0'; // see workaround below
+    //buf[n+1] = '\0'; // see workaround below
 
-  // Send buffer in hex
-  char messagestart[128];
-  char* messageptr = messagestart;
-  sprintf(messagestart, "DEBUG:  Send buffer hex bytes:  ( ");
-  messageptr += strlen(messagestart);
-
-  for (int j = 0; j < n; j++) {
-    int size = sprintf(messageptr, "%02x ", buf[j]);
-    messageptr += 3;
-  }
-  lprintf("%s)", messagestart);
-
-  /* The below command doesn't take more than an 8-byte payload 5 chars (+ 3
-     bytes of <CRC><CRC><CR>).  It has to do with low speed USB specifications.
-     So we must chunk up the data and send it in a loop 8 bytes at a time.  */
-
-  // Send the command (or part of the command if longer than chunk_size)
+     // Send the command (or part of the command if longer than chunk_size)
   int chunk_size = 8;
   if (n < chunk_size) // Send in chunks of 8 bytes, if less than 8 bytes to send... just send that
     chunk_size = n;
@@ -173,54 +157,53 @@ bool cInverter::query(const char *cmd) {
 
   time(&started);
 
-  // Instead of using a fixed size for expected response length, lets find it
-  // by searching for the first returned <cr> char instead.
-  char *startbuf = 0;
-  char *endbuf = 0;
-  do {
-    // According to protocol manual, it appears no query should ever exceed 120 byte size in response
-    n = read(fd, (void*)buf+i, 120 - i);
-    if (n < 0)
-    {
-      if (time(NULL) - started > 5)     // Wait 5 secs before timeout
-      {
-        lprintf("DEBUG:  %s read timeout", cmd);
-        break;
-      }
-      else
-      {
-        usleep(50000);  // sleep 50ms
-        continue;
-      }
-    }
-    i += n;
-    buf[i] = '\0';  // terminate what we have so far with a null string
-    lprintf("DEBUG:  %d bytes read, %d total bytes:  %02x %02x %02x %02x %02x %02x %02x %02x",
-            n, i, buf[i-8], buf[i-7], buf[i-6], buf[i-5], buf[i-4], buf[i-3], buf[i-2], buf[i-1]);
+	
+    //read a command
+    char *startbuf = 0;
+    char *endbuf = 0;
+    time(&started);
+    do {
+        n = read(fd, &buf[i], 120-i);
+        if (n < 0) {
+            if (time(NULL) - started > 2) {
+                lprintf("INVERTER: %s read timeout", cmd);
+                break;
+            } else {
+                usleep(10);
+                continue;
+            }
+        }
 
+        i += n;
     startbuf = (char *)&buf[0];
-    endbuf = strchr(startbuf, '\r');
+	endbuf = strchr(startbuf, '\r');
+    } while (endbuf == NULL);
+    close(fd);
+    buf[i] = '\0';
 
-    //lprintf("DEBUG:  %s Current buffer: %s", cmd, startbuf);
-  } while (endbuf == NULL);     // Still haven't found end <cr> char as long as pointer is null
-  close(fd);
+     if (endbuf != NULL) {
+        int replysize = endbuf - startbuf + 1;
 
-  int replysize = endbuf - startbuf + 1;
-  lprintf("DEBUG:  Found reply <cr> at byte: %d", replysize);
+        lprintf("INVERTER: %s reply size (%d bytes)", cmd, i);
 
-  if (buf[0]!='(' || buf[replysize-1]!=0x0d) {
-    lprintf("DEBUG:  %s: incorrect buffer start/stop bytes.  Buffer: %s", cmd, buf);
-    return false;
-  }
-  if (!(CheckCRC(buf, replysize))) {
-    lprintf("DEBUG:  %s: CRC Failed!  Reply size: %d  Buffer: %s", cmd, replysize, buf);
-    return false;
-  }
-  buf[replysize-3] = '\0';      // Null-terminating on first CRC byte
-  lprintf("DEBUG:  %s: %d bytes read: %s", cmd, i, buf);
-  
-  lprintf("DEBUG:  %s query finished", cmd);
-  return true;
+        if (buf[0]!='(' || buf[replysize-1]!=0x0d) {
+            lprintf("INVERTER: %s: incorrect start/stop bytes.  Buffer: %s", cmd, buf);
+            return false;
+        }
+        if (!(CheckCRC(buf, replysize))) {
+            lprintf("INVERTER: %s: CRC Failed!  Reply size: %d  Buffer: %s", cmd, replysize, buf);
+            return false;
+        }
+
+        buf[replysize-3] = '\0'; // null terminating on first CRC byte
+        lprintf("INVERTER: %s: %d bytes read: %s", cmd, i, buf);
+
+        lprintf("INVERTER: %s query finished", cmd);
+        return true;
+    } else {
+        lprintf("INVERTER: %s couldn't find reply <cr> (%d bytes)", cmd, i);
+        return false;
+    }
 }
 
 void cInverter::poll() {
